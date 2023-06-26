e52154414c8c571f050d52d7a90d46d046f04da3
Tonistark92 <hanyislam270@gmail.com>
2023-06-26 14:08:18 -0700

    task #1 1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,3 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program for change value with pointer
+ *
+ * Return: void
+ *
+*/
+
 void reset_to_98(int *n)
 {
 	*n = 98;
