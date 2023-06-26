503313a81b0481e469a575dcbe0fd91a4a47ae83
Tonistark92 <hanyislam270@gmail.com>
2023-06-26 15:35:53 -0700

    task 5#

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strlen - function implementation
+ *
+ * Description: A C program to swap two params
+ * @s: the number to swap
+ *
+ * Return: void
+ *
+*/
+
+void print_rev(char *s);
+
+{
+
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+	length++;
+	}
+
+	while (s[length] != 0)
+	{
+		_putchar(*(str + i));
+		length--;
+	}
+	_putchar('\n');
+
+}
