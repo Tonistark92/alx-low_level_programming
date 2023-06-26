fa2316df29db3c72670c1b90f3bcac6457ca5ec8
Tonistark92 <hanyislam270@gmail.com>
2023-06-26 14:50:36 -0700

    #2 task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * swap_int - function implementation
+ *
+ * Description: A C program to swap two params
+ * @a: the number to swap 
+ * @b: the number to swap
+ *
+ * Return: void
+ *
+*/
+
+void swap_int(int *a, int *b)
+{
+	int *temp;
+	temp = a;
+	a = b ;
+        b = temp;	
+	
+}
