a6a36c4d5898a2fce810873778991250eeac6552
Tonistark92 <hanyislam270@gmail.com>
2023-06-26 15:15:53 -0700

    task 3#

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strlen - function implementation
+ *
+ * Description: A C program to swap two params
+ * @s: the number to swap
+ *
+ * Return: void
+ *
+*/
+
+int _strlen(char *s)
+
+{
+
+	int length = 0;
+
+	while (str[length] != '\0')
+	{
+	length++;
+	}
+
+	return (length);
+
+}
